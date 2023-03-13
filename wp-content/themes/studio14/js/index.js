84a812f0bb26b6410a222ac8ff27f2a029a000d9
Samuel-Fortune <samuelchibu04@gmail.com>
2023-03-14 00:27:19 +0100

    wp-studio14

--- wp-content/themes/studio14/js/index.js
+++ wp-content/themes/studio14/js/index.js
@@ -3,11 +3,28 @@ window.onload = function() {
 
 	$( '#click' ).click(function( e ) {
 		e.preventDefault();
-		$('#mobile-list-menu').addClass('mobile-list-menu');
+		$('#mobile-list-menu').toggle('');
+		$('nav').css('align-items', 'flex-start');
+		$('#mobile-list-menu').css({
+			'display': 'flex',
+			'width': '500px',
+			'height': '1000px',
+			'overflow': 'hidden',
+			'background-color': 'red',
+			'justify-content': 'center'
+		});
+		$('.mobile-menu nav-menu').css({
+			'display': 'flex',
+			'flex-direction': 'column',
+			'margin': 'auto',
+			'height': '400px',
+			'justify-content': 'space-between'
+		});
 	} );
 
-	$( '#click' ).click(function( e ) {
-		e.preventDefault();
-		$('#mobile-list-menu').removeClass('mobile-list');
-	} );
+	$(window).resize(function() {
+		if ($(window).width() > 768) {
+			location.reload();
+		}
+	});
 };
