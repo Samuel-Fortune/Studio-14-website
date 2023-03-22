c8cee465b85eb5e9a769dff505219cf6fcb9e824
Samuel-Fortune <samuelchibu04@gmail.com>
2023-03-22 23:04:52 +0100

    wp-studio

--- wp-content/themes/studio14/header.php
+++ wp-content/themes/studio14/header.php
@@ -26,7 +26,7 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'studio14' ); ?></a>
 
-	<header id="masthead" class="site-header">
+	<h  eader id="masthead" class="site-header">
 		<!--NAV-->
 		<div class="nav">
 			<div class="site-wrapper">
@@ -80,4 +80,4 @@
 
 			</div>
 		</div>
-	</header><!-- #masthead -->
+	</h><!-- #masthead -->
